697098a140df09c62e408a35de57d844e52a2308
noor <noormedia82@gmail.com>
2019-11-05 15:23:54 +0100

    Updates

---
+++ precache-manifest.6f068b243e4e4319b8371bce6166a564.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d008dee3ce167b611e6edd9e5cabb4d6",
+    "url": "/userlogin/index.html"
+  },
+  {
+    "revision": "a85117d9bd067f45791a",
+    "url": "/userlogin/static/css/main.18788222.chunk.css"
+  },
+  {
+    "revision": "8c8a6b5d95945de37e80",
+    "url": "/userlogin/static/js/2.66e327c5.chunk.js"
+  },
+  {
+    "revision": "a85117d9bd067f45791a",
+    "url": "/userlogin/static/js/main.0add3216.chunk.js"
+  },
+  {
+    "revision": "febf3caa7f88e1c72846",
+    "url": "/userlogin/static/js/runtime-main.876eea80.js"
+  }
+]);
